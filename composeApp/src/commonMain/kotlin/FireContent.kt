0f185cb3643bcc0b9373c3268ec2518c65839700
Nicholas DiPatri <ndipatri@gmail.com>
2024-04-30 20:25:33 -0400

    in-file attribution for Adam.

--- composeApp/src/commonMain/kotlin/FireContent.kt
+++ composeApp/src/commonMain/kotlin/FireContent.kt
@@ -18,6 +18,17 @@ import kotlin.random.Random
 /**
  *  Draws a rectangle of fire elements, each varying in color from black
  *  to bright orange.. thus simulating the look of an active fire.
+ *
+ *  See https://fabiensanglard.net/doom_fire_psx/
+ *
+ * HUUUGE thanks to https://linkedin.com/in/adam-bennett-4784b327 for his original Doom Fire Compose Repo:
+ * https://github.com/ditn/Doom-Compose
+ *
+ * I've just taken Adam's work (which he did back when Compose was in its infancy),
+ * and introduced some newer paradigms.. including Compose Multiplatform.
+ * Now this Doom Fire effect will work on Android and iOS!
+ *
+ * Learn more about [Kotlin Multiplatform](https://www.jetbrains.com/help/kotlin-multiplatform-dev/get-started.html)…# doomFire
  */
 @Composable
 fun FireContent(
